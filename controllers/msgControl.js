74ca3e00717dd82581e067aeacd7e1dac7c90edf
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-19 20:31:10 -0500

    andando

--- controllers/msgControl.js
+++ controllers/msgControl.js
@@ -1,18 +1,18 @@
-const  messages  = require("../configDB");
-const { CHANNEL_SLACK } = process.env;
+const  {messages}  = require("../configDB");
+const { CHANNEL_SLACK, SLACK_TOCKEN} = process.env;
 const { WebClient } = require('@slack/web-api');
 
 
 // Crear instancia de WebClient de Slack
-const slackToken = process.env.SLACK_TOKEN;
+const slackToken = SLACK_TOCKEN
 const web = new WebClient(slackToken);  // recibiendo el token de slack api
 
 
 const msgControl = async (req,res) => {
+    console.log("slack tockeeeeeeeeeen =", slackToken, "Chanel slaaaaaaaaaaaaaaack",CHANNEL_SLACK)
 try {
     // Obtener mensajes del canal de Slack
-    const result = await web.conversations.history({ CHANNEL_SLACK });
-    console.log("HOLAAAAAAA",result)
+    const result = await web.conversations.history({ channel: CHANNEL_SLACK });
 
     // Guardar mensajes en la base de datos
     for (const message of result.messages) {
